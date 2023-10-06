eb34d03fd0c6b8fc87f5ff807cdcfddd62dbc212
emilyallegretti <emmy.allegretti@gmail.com>
2023-10-06 16:27:17 -0400

    test: test data

--- CoreTokenizerMain.py
+++ CoreTokenizerMain.py
@@ -1,20 +1,25 @@
 import sys
-import Scanner
+from Scanner import *
 
 # main driver function for the Core Tokenizer.
 # expects 1 command line argument: the name of the input file.
 # this function prints out the list of tokens scanned from the input file with Scanner.
 def main(args):
+    print(args[1])
     scanner = Scanner(args[1])
     nextToken = scanner.getToken()
     # repeatedly print out token and get next one in scanner,
     # until end-of-file or invalid token is reached
     while (1):
-        print(nextToken + "/n")
+        print(nextToken)
         if nextToken < 33:
             scanner.skipToken()
-            scanner.getToken()
+            nextToken = scanner.getToken()
         else:
+            if nextToken == 33:
+                print('End of file reached.')
+            else:
+                print("ERROR: Invalid Token. Scanner aborted.")
             break
     
 
--- Scanner.py
+++ Scanner.py
@@ -1,20 +1,21 @@
+import sys
 from Token import *
 
 # A Scanner for the Core language that tokenizes each line of a given input file into tokens for the Core language, stored in a private data structure that '
 # can be iterated through and accessed with instance methods getToken(), skipToken(), intVal(), and idName(). 
 class Scanner:
-    error_message = "ERROR: Invalid Token. Scanner aborted"
     # Constructor.
     def __init__(self, filename): 
         # open file and tokenize line-by-line into tokens
-        with open(filename, 'r') as self.input:
-            self._tokens = []
-            self._tokenizeLine(self.input)
-            # initialize cursor to first token index
-            self._cursor = 0
-            # initialize a Map that will hold the string or numerical value for identifiers and integers, respectively. 
-            # the key will be the index of that integer or identifier's location in the tokens array, and the value will be its actual value. 
-            self.actual_values = {}
+        self.input = open(filename, 'r')
+        self._tokens = []
+        # initialize cursor to first token index
+        self._cursor = 0
+        # initialize a Map that will hold the string or numerical value for identifiers and integers, respectively. 
+        # the key will be the index of that integer or identifier's location in the tokens array, and the value will be its actual value. 
+        self.actual_values = {}
+        # tokenize first line
+        self._tokenizeLine()
 
     # Tokenizes a line from the input file into tokens array. 
     def _tokenizeLine(self): 
@@ -23,35 +24,34 @@ class Scanner:
         # check if the line is entirely whitespace; if so, continue to read lines until one without whitespace is found
         while line.isspace():
             line = (self.input).readline()
-         # check if we've reached the end of the file, append EOF token if so and return
+         # ch  eck if we've reached the end of the file, append EOF token if so and return
         if line == "":
            self._tokens.append(Token.EOF)
-           print("End of file reached.")
+           self.input.close()   # close file
            return
         # analyze each character in the line to determine its token
         i=0
         while i < len(line):
             currChar = line[i]
+
         # CASES:
          # WHITESPACE
          # if whitespace character is found, continue iterating through line until next non-whitespace is found
-            while currChar.isspace():
+            if currChar.isspace():
                i=i+1
-               currChar=line[i]
-
         # INTEGERS
-            if currChar.isdigit():
+            elif currChar.isdigit():
                # begin building string of digits now that we've come across one decimal digit--digitString will hold this string
                digitString=currChar
                #iterate through line until next character is not a digit
                i = i+1
-               while (i<len(line) & line[i].isdigit()):
+               while (i<len(line) and line[i].isdigit()):
                   digitString=digitString+line[i]
                   i=i+1
               # check if current character is a letter (not a special character)--if so, this is an illegal token and scanner should stop reading.
-               if i<len(line) & line[i].isalpha():
+               if i<len(line) and line[i].isalpha():
                   self._tokens.append(Token.INVALID)
-                  print(self.error_message)
+                  
                   break
             # if we've gotten to this point, digitString is a legal string of decimal digits
             # add its numeric value to actual_values and update tokens array
@@ -64,13 +64,13 @@ class Scanner:
                idString=currChar
                # iterate through line until next character is not an uppercase letter or digit
                i = i+1
-               while (i < len(line)) & (line[i].isdigit() | line[i].isupper()):
+               while (i < len(line)) and (line[i].isdigit() or line[i].isupper()):
                   idString = idString+line[i]
                   i = i+1
                # check if current character is a lowercase letter (not a special character)--if so, this is an illegal token and scanner should stop reading.
-               if i < len(line) & line[i].islower():
+               if i < len(line) and line[i].islower():
                   self._tokens.append(Token.INVALID)
-                  print(self.error_message)
+                  
                   break
                # if we've gotten to this point, idString is a legal identifier
                # add its string value to actual_values and update tokens array
@@ -118,52 +118,56 @@ class Scanner:
             elif currChar=="&":
                # check to see if next character is also an ampersand; if not, this token is illegal
                i=i+1
-               if (i<len(line) & line[i] == "&"):
+               if (i<len(line) and line[i] == "&"):
                   self._tokens.append(Token.AND)
+                  i=i+1
                else:
-                  self._tokens.append(Token.INVALID)
-                  print(self.error_message)
+                  self._tokens.append(Token.INVALID)  
                   break
                
             elif currChar == "|":
                # check to see if next character is also a vertical bar; if not, this token is illegal
                i = i+1
-               if (i < len(line) & line[i] == "|"):
+               if (i < len(line) and line[i] == "|"):
                   self._tokens.append(Token.OR)
+                  i=i+1
                else:
                   self._tokens.append(Token.INVALID)
-                  print(self.error_message)
                   break
                
             elif currChar == "!":
                # check to see if next character is an equals sign for 'greedy tokenizing'
                i = i+1
-               if (i < len(line) & line[i] == "="):
+               if (i < len(line) and line[i] == "="):
                   self._tokens.append(Token.NOT_EQUALS)
+                  i=i+1
                else: # otherwise just add not symbol
                   self._tokens.append(Token.NOT)
 
             elif currChar == ">":
                # check to see if next character is an equals sign for 'greedy tokenizing'
                i = i+1
-               if (i < len(line) & line[i] == "="):
+               if (i < len(line) and line[i] == "="):
                   self._tokens.append(Token.GT_EQUALS)
+                  i=i+1
                else: #otherwise just add GT symbol
                   self._tokens.append(Token.GREATER_THAN)
 
             elif currChar == "<":
                # check to see if next character is an equals sign for 'greedy tokenizing'
                i = i+1
-               if (i < len(line) & line[i] == "="):
+               if (i < len(line) and line[i] == "="):
                   self._tokens.append(Token.LT_EQUALS)
+                  i=i+1
                else: #otherwise just add LT symbol
                   self._tokens.append(Token.LESS_THAN)
 
             elif currChar == "=":
                # check to see if next character is an equals sign for 'greedy tokenizing'
                i = i+1
-               if (i < len(line) & line[i] == "="):
+               if (i < len(line) and line[i] == "="):
                   self._tokens.append(Token.EQUALS)
+                  i=i+1
                else: #otherwise just add single equals symbol
                   self._tokens.append(Token.ASSIGN)
 
@@ -172,16 +176,15 @@ class Scanner:
             elif currChar == "p":
                # check to see if rest of characters make up 'program', otherwise we have an illegal token
                substring='rogram'
+               i=i+1
                for letter in substring:
-                  i=i+1
-                  if i==len(line) | line[i] != letter:
-                     print(self.error_message)
+                  if (i==len(line)) or (line[i] != letter):
                      self._tokens.append(Token.INVALID)
                      break
                   else:
                      i=i+1
                 # break out of outer loop if 'program' wasn't found, otherwise add program token to array
-               if self._tokens[-1]==Token.INVALID:
+               if (len(self._tokens) > 0) and (self._tokens[-1]==Token.INVALID):
                   break
                else: 
                   self._tokens.append(Token.PROGRAM)
@@ -192,14 +195,13 @@ class Scanner:
                i=i+1
                substring='egin'
                for letter in substring:
-                  if i==len(line) | line[i] != letter:
-                     print(self.error_message)
+                  if i==len(line) or line[i] != letter:
                      self._tokens.append(Token.INVALID)
                      break
                   else:
                      i=i+1
                 # break out of outer loop if 'begin' wasn't found, otherwise add begin token to array
-               if self._tokens[-1]==Token.INVALID:
+               if len(self._tokens) > 0 and self._tokens[-1]==Token.INVALID:
                   break
                else: 
                   self._tokens.append(Token.BEGIN)
@@ -210,14 +212,14 @@ class Scanner:
                i=i+1
                substring='hen'
                for letter in substring:
-                  if i==len(line) | line[i] != letter:
-                     print(self.error_message)
+                  if i==len(line) or line[i] != letter:
+                     
                      self._tokens.append(Token.INVALID)
                      break
                   else:
                      i=i+1
                 # break out of outer loop if 'then' wasn't found, otherwise add then token to array
-               if self._tokens[-1]==Token.INVALID:
+               if len(self._tokens) > 0 and self._tokens[-1]==Token.INVALID:
                   break
                else: 
                   self._tokens.append(Token.THEN)
@@ -228,14 +230,14 @@ class Scanner:
                i=i+1
                substring='oop'
                for letter in substring:
-                  if i==len(line) | line[i] != letter:
-                     print(self.error_message)
+                  if i==len(line) or line[i] != letter:
+                     
                      self._tokens.append(Token.INVALID)
                      break
                   else:
                      i=i+1
                 # break out of outer loop if 'loop' wasn't found, otherwise add loop token to array
-               if self._tokens[-1]==Token.INVALID:
+               if len(self._tokens) > 0 and self._tokens[-1]==Token.INVALID:
                   break
                else: 
                   self._tokens.append(Token.LOOP)
@@ -246,14 +248,14 @@ class Scanner:
                i=i+1
                substring='ead'
                for letter in substring:
-                  if i==len(line) | line[i] != letter:
-                     print(self.error_message)
+                  if i==len(line) or line[i] != letter:
+                     
                      self._tokens.append(Token.INVALID)
                      break
                   else:
                      i=i+1
                 # break out of outer loop if 'read' wasn't found, otherwise add read token to array
-               if self._tokens[-1]==Token.INVALID:
+               if len(self._tokens) > 0 and self._tokens[-1] == Token.INVALID:
                   break
                else: 
                   self._tokens.append(Token.READ)
@@ -264,31 +266,31 @@ class Scanner:
                # if next char is 'r', test to see if we have write keyword.
                # otherwise, we have an illegal token.
                i=i+1
-               if i< len(line) & line[i]=="h":
+               if i< len(line) and line[i]=="h":
                   substring="hile"
                   for letter in substring:
-                    if i == len(line) | line[i] != letter:
-                        print(self.error_message)
+                    if i == len(line) or line[i] != letter:
+                        
                         self._tokens.append(Token.INVALID)
                         break
                     else: 
                        i=i+1
                 # break out of outer loop if 'while' wasn't found, otherwise add while token to array
-                  if self._tokens[-1] == Token.INVALID:
+                  if len(self._tokens) > 0 and self._tokens[-1] == Token.INVALID:
                      break
                   else:
                      self._tokens.append(Token.WHILE)
-               elif i<len(line) & line[i]=="r":
+               elif i<len(line) and line[i]=="r":
                   substring = "rite"
                   for letter in substring:
-                    if i == len(line) | line[i] != letter:
-                        print(self.error_message)
+                    if i == len(line) or line[i] != letter:
+                        
                         self._tokens.append(Token.INVALID)
                         break
                     else:
                        i = i+1
                   # break out of outer loop if 'write' wasn't found, otherwise add write token to array
-                  if self._tokens[-1] == Token.INVALID:
+                  if len(self._tokens) > 0 and self._tokens[-1] == Token.INVALID:
                      break
                   else:
                      self._tokens.append(Token.WRITE)
@@ -303,20 +305,18 @@ class Scanner:
                # if next char is 'f', we have an if keyword.
                # otherwise, we have an illegal token.
                i = i+1
-               if i<len(line) & line[i] == "n":
+               if i<len(line) and line[i] == "n":
                   i=i+1
-                  if i<len(line) & line[i] == "t":
+                  if i<len(line) and line[i] == "t":
                     self._tokens.append(Token.INT)
                     i=i+1
                   else:
-                    print(self._error)
                     self._tokens.append(Token.INVALID)
                     break
-               elif i<len(line) & line[i] == "f":
+               elif i<len(line) and line[i] == "f":
                   self._tokens.append(Token.IF)
                   i=i+1
                else:
-                  print(self._error)
                   self._tokens.append(Token.INVALID)
                   break
 
@@ -326,35 +326,37 @@ class Scanner:
                # if next char is 'n', test to see if we have end keyword.
                # otherwise, we have an illegal token.
                i = i+1
-               if i<len(line) & line[i] == "l":
+               if i<len(line) and line[i] == "l":
                    substring = "lse"
                    for letter in substring:
-                    if i == len(line) | line[i] != letter:
-                        print(self.error_message)
+                    if i == len(line) or line[i] != letter:
+                        
                         self._tokens.append(Token.INVALID)
                         break
                     else:
                        i = i+1
                   # break out of outer loop if 'else' wasn't found, otherwise add else token to array
-                   if self._tokens[-1] == Token.INVALID:
+                   if (len(self._tokens) > 0) and self._tokens[-1] == Token.INVALID:
                      break
                    else:
                      self._tokens.append(Token.ELSE)
 
-               elif i<len(line) & line[i] == "n":
+               elif i<len(line) and line[i] == "n":
                   i=i+1
-                  if i < len(line) & line[i] == "d":
+                  if i < len(line) and line[i] == "d":
                     self._tokens.append(Token.END)
                     i = i+1
                   else:
-                    print(self._error)
                     self._tokens.append(Token.INVALID)
                     break
 
                else:
-                  print(self._error)
                   self._tokens.append(Token.INVALID)
                   break
+           # if no matching tokens found, then this is an invalid token
+            else: 
+               self._tokens.append(Token.INVALID)
+               break
 
 
                
@@ -368,17 +370,30 @@ class Scanner:
     # by cursor is 33 (EOF) or 34 (illegal token), in which the cursor is not moved. 
     # If advancing the cursor forward puts it past the last index of the tokens array, the next line from the file is 
     # tokenized into tokens array with tokenizeLine(). 
-    def _skipToken(self):
+    def skipToken(self):
        if self.getToken() < 33:
-        self._cursor=self.cursor+1
+        self._cursor=self._cursor+1
         if self._cursor == len(self._tokens):
-         self._tokenizeLine(self)
+         self._tokenizeLine()
 
     # Returns the token number that is being pointed to in tokens array by cursor.
     def getToken(self):
-       return self._tokens[self._cursor]
-
+       return self._tokens[self._cursor].value
+    
+    # Returns the value of the integer token being pointed to by cursor.
+    # If the current token is not an integer, program will exit.
     def intVal(self):
-       return
+       if self.getToken() == 31:
+          return self.actual_values[self._cursor]
+       else:
+          print("ERROR: current token is not an integer. Terminating program...")
+          sys.exit(1)
+
+    # Returns the value of the identifier token being pointed to by cursor.
+    # If the current token is not an identifier, program will exit.
     def idName(self):
-       return
+       if self.getToken() == 32:
+          return self.actual_values[self._cursor]
+       else:
+          print("ERROR: current token is not an identifier. Terminating program...")
+          sys.exit(1)
